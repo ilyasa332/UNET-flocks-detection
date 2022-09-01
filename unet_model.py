b19decc871b292cc7fdd9f218ef06cd0dbb2017b
Inbal Schekler <goldinbal@gmail.com>
2022-09-01 16:31:46 +0300

    Add files via upload

---
+++ unet_model.py
@@ -0,0 +1,59 @@
+import keras
+from keras.models import Model
+from keras.layers import Conv2D, MaxPooling2D, Input, Conv2DTranspose, Concatenate, BatchNormalization, UpSampling2D
+import tensorflow as tf
+
+def mean_iou(y_true, y_pred):
+    yt0 = y_true[:,:,:,0]
+    yp0 = K.cast(y_pred[:,:,:,0] > 0.5, 'float32')
+    inter = tf.count_nonzero(tf.logical_and(tf.equal(yt0, 1), tf.equal(yp0, 1)))
+    union = tf.count_nonzero(tf.add(yt0, yp0))
+    iou = tf.where(tf.equal(union, 0), 1., tf.cast(inter/union, 'float32'))
+    return iou
+
+
+def unet(sz=(256, 256, 9)):
+    x = Input(sz)
+    inputs = x
+
+    # down sampling
+    f = 8
+    layers = []
+
+    for i in range(0, 6):
+        x = Conv2D(f, 3, activation='relu', padding='same')(x)
+        x = Conv2D(f, 3, activation='relu', padding='same')(x)
+        layers.append(x)
+        x = MaxPooling2D()(x)
+        f = f * 2
+    ff2 = 64
+
+    # bottleneck
+    j = len(layers) - 1
+    x = Conv2D(f, 3, activation='relu', padding='same')(x)
+    x = Conv2D(f, 3, activation='relu', padding='same')(x)
+    x = Conv2DTranspose(ff2, 2, strides=(2, 2), padding='same')(x)
+    x = Concatenate(axis=3)([x, layers[j]])
+    j = j - 1
+
+    # upsampling
+    for i in range(0, 5):
+        ff2 = ff2 // 2
+        f = f // 2
+        x = Conv2D(f, 3, activation='relu', padding='same')(x)
+        x = Conv2D(f, 3, activation='relu', padding='same')(x)
+        x = Conv2DTranspose(ff2, 2, strides=(2, 2), padding='same')(x)
+        x = Concatenate(axis=3)([x, layers[j]])
+        j = j - 1
+
+        # classification
+    x = Conv2D(f, 3, activation='relu', padding='same')(x)
+    x = Conv2D(f, 3, activation='relu', padding='same')(x)
+    outputs = Conv2D(1, 1, activation='sigmoid')(x)
+
+    ##changed the metrics in the compile to accuracy
+    # model creation
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=[mean_iou, 'accuracy'])
+
+    return model
