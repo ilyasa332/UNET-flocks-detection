299551fab2b32ac99a0577da63f072c001a20541
Inbal Schekler <goldinbal@gmail.com>
2022-09-06 19:01:49 +0300

    Update unet_model.py

--- unet_model.py
+++ unet_model.py
@@ -2,15 +2,9 @@ import keras
 from keras.models import Model
 from keras.layers import Conv2D, MaxPooling2D, Input, Conv2DTranspose, Concatenate, BatchNormalization, UpSampling2D
 import tensorflow as tf
-from keras import backend as K
 
-def mean_iou(y_true, y_pred):
-    yt0 = y_true[:,:,:,0]
-    yp0 = K.cast(y_pred[:,:,:,0] > 0.5, 'float32')
-    inter = tf.count_nonzero(tf.logical_and(tf.equal(yt0, 1), tf.equal(yp0, 1)))
-    union = tf.count_nonzero(tf.add(yt0, yp0))
-    iou = tf.where(tf.equal(union, 0), 1., tf.cast(inter/union, 'float32'))
-    return iou
+
+
 
 
 def unet(sz=(256, 256, 9)):
